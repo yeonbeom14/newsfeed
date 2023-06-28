3c5fece6bb0429c53b9d50b24447593194c0c071
mingyu kim9411 <awer159@daum.net>
2023-06-28 14:35:45 +0900

    변경점

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ const cookieParser = require("cookie-parser");
 const usersRouter = require("./routes/users.js");
 const postsRouter = require("./routes/posts.js");
 const commentsRouter = require("./routes/comments.js");
+const cors=require("cors")
 const path = require('path');
 const app = express();
 const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use('/api', [usersRouter, postsRouter, commentsRouter]);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./html-newsfeed/main.html')); 
-  });
-
-
-  app.get('/users', (req, res) => {
-    res.sendFile(path.join(__dirname,'./html-newsfeed/regist.html')); 
-  });
 
 app.listen(PORT, () => {
     console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
